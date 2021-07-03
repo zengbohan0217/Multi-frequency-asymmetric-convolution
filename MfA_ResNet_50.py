de3de727fd368477a71f2f66c87c9fb42d4a6b01
zengbohan0217 <13811753844@126.com>
2021-07-03 18:55:19 +0800

    update resnet_50

--- MfA_ResNet_50.py
+++ MfA_ResNet_50.py
@@ -59,5 +59,55 @@ class IndentityBlock(nn.Module):
         x = self.relu_1(x)
         return x
 
+class ResModule(nn.Module):
+    def __init__(self, in_channels, n_class):
+        super(ResModule, self).__init__()
+        self.stage1 = nn.Sequential(
+            MfA_Conv(in_channels, 16, high_L=5, high_S=3, low_L=7, low_S=5,
+                     basic_stride=2, high_stride=2, low_stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.MaxPool2d(3, 2, padding=1),
+        )
+        self.stage2 = nn.Sequential(
+            ConvBlock(64, kernels=[5, 1, 5, 3], filters=[16, 16, 64], s=1),
+            IndentityBlock(256, kernels=[5, 1, 5, 3], filters=[16, 16, 64]),
+            IndentityBlock(256, kernels=[5, 1, 5, 3], filters=[16, 16, 64]),
+        )
+        self.stage3 = nn.Sequential(
+            ConvBlock(256, kernels=[5, 1, 5, 3], filters=[32, 32, 128], s=2),
+            IndentityBlock(512, kernels=[5, 1, 5, 3], filters=[32, 32, 128]),
+            IndentityBlock(512, kernels=[5, 1, 5, 3], filters=[32, 32, 128]),
+            IndentityBlock(512, kernels=[5, 1, 5, 3], filters=[32, 32, 128]),
+        )
+        self.stage4 = nn.Sequential(
+            ConvBlock(512, kernels=[5, 1, 5, 3], filters=[64, 64, 256], s=2),
+            IndentityBlock(1024, kernels=[5, 1, 5, 3], filters=[64, 64, 256]),
+            IndentityBlock(1024, kernels=[5, 1, 5, 3], filters=[64, 64, 256]),
+            IndentityBlock(1024, kernels=[5, 1, 5, 3], filters=[64, 64, 256]),
+            IndentityBlock(1024, kernels=[5, 1, 5, 3], filters=[64, 64, 256]),
+            IndentityBlock(1024, kernels=[5, 1, 5, 3], filters=[64, 64, 256]),
+        )
+        self.stage5 = nn.Sequential(
+            ConvBlock(1024, kernels=[5, 1, 5, 3], filters=[128, 128, 512], s=2),
+            IndentityBlock(2048, kernels=[5, 1, 5, 3], filters=[128, 128, 512]),
+            IndentityBlock(2048, kernels=[5, 1, 5, 3], filters=[128, 128, 512]),
+        )
+        self.pool = nn.AvgPool2d(2, 2, padding=1)
+        self.fc = nn.Sequential(
+            nn.Linear(8192, n_class)
+        )
 
+    def forward(self, X):
+        out = self.stage1(X)
+        out = self.stage2(out)
+        out = self.stage3(out)
+        out = self.stage4(out)
+        out = self.stage5(out)
+        out = self.pool(out)
+        out = self.pool(out)
+        out = self.pool(out)
+        out = out.view(out.size(0), 8192)
+        out = self.fc(out)
+        return out
 
