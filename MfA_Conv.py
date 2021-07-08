a3dc731efd84642bd85684a90eb212e6ea118280
zengbohan0217 <13811753844@126.com>
2021-07-08 15:20:46 +0800

    a little bit change

--- MfA_Conv.py
+++ MfA_Conv.py
@@ -37,16 +37,16 @@ class MfA_Conv(nn.Module):
                                        stride=(low_stride, basic_stride), padding=(self.padding_ls, self.padding_ll))
         self.Vert_low_conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(low_L, low_S),
                                        stride=(low_stride, basic_stride), padding=(self.padding_ll, self.padding_ls))
-        # self.weight_hh = nn.Parameter(torch.FloatTensor(2))
-        # self.weight_hh.data = F.softmax(self.weight_hh, dim=0)  # attention this weight.data
-        # self.weight_ll = nn.Parameter(torch.FloatTensor(2))
-        # self.weight_ll.data = F.softmax(self.weight_ll, dim=0)
-        # self.weight_hl = nn.Parameter(torch.FloatTensor(2))
-        # self.weight_hl.data = F.softmax(self.weight_hl, dim=0)
-        # self.weight_lh = nn.Parameter(torch.FloatTensor(2))
-        # self.weight_lh.data = F.softmax(self.weight_lh, dim=0)
-        self.weight = nn.Parameter(torch.FloatTensor(4))
-        self.weight.data = F.softmax(self.weight, dim=0)  # attention this weight.data
+        self.weight_hh = nn.Parameter(torch.FloatTensor(2))
+        self.weight_hh.data = F.softmax(self.weight_hh, dim=0)  # attention this weight.data
+        self.weight_ll = nn.Parameter(torch.FloatTensor(2))
+        self.weight_ll.data = F.softmax(self.weight_ll, dim=0)
+        self.weight_hl = nn.Parameter(torch.FloatTensor(2))
+        self.weight_hl.data = F.softmax(self.weight_hl, dim=0)
+        self.weight_lh = nn.Parameter(torch.FloatTensor(2))
+        self.weight_lh.data = F.softmax(self.weight_lh, dim=0)
+        # self.weight = nn.Parameter(torch.FloatTensor(4))
+        # self.weight.data = F.softmax(self.weight, dim=0)  # attention this weight.data
         # self.alpha, self.beta, self.delta, self.gamma = self.get_weight()
 
     def count_padding(self):
@@ -72,8 +72,8 @@ class MfA_Conv(nn.Module):
                        weight_2 * 0.25 * feature_map_2[:, 0:up_1_size+1, :, :]
         up_2_feature = weight_1 * 0.25 * feature_map_1[:, up_1_size+1:up_2_size+1, :, :] + \
                        weight_2 * 0.75 * feature_map_2[:, up_1_size+1:up_2_size+1, :, :]
-        equal_feature = weight_1 * 0.25 * feature_map_1[:, up_2_size+1:self.out_channel+1, :, :] + \
-                       weight_2 * 0.75 * feature_map_2[:, up_2_size+1:self.out_channel+1, :, :]
+        equal_feature = weight_1 * 0.5 * feature_map_1[:, up_2_size+1:self.out_channel+1, :, :] + \
+                       weight_2 * 0.5 * feature_map_2[:, up_2_size+1:self.out_channel+1, :, :]
         final_out = torch.cat([up_1_feature, up_2_feature, equal_feature], 1)
         return final_out
 
@@ -83,14 +83,18 @@ class MfA_Conv(nn.Module):
         out_Vert_h = self.Vert_high_conv(x)
         out_Horn_l = self.Hori_low_conv(x)
         out_Vert_l = self.Vert_low_conv(x)
-        # out_hh = self.weight_hh[0] * out_Vert_h + self.weight_hh[1] * out_Horn_h
-        # out_ll = self.weight_ll[0] * out_Vert_l + self.weight_ll[1] * out_Horn_l
-        # out_hl = self.weight_hl[0] * out_Vert_h + self.weight_hl[1] * out_Horn_l
-        # out_lh = self.weight_lh[0] * out_Vert_l + self.weight_lh[1] * out_Horn_h
-        out_hh = self.feature_map_add(self.weight[0], self.weight[1], out_Vert_h, out_Horn_h)
-        out_ll = self.feature_map_add(self.weight[2], self.weight[3], out_Vert_l, out_Horn_l)
-        out_hl = self.feature_map_add(self.weight[0], self.weight[3], out_Vert_h, out_Horn_l)
-        out_lh = self.feature_map_add(self.weight[2], self.weight[1], out_Vert_l, out_Horn_h)
+        self.weight_hh.data = F.softmax(self.weight_hh, dim=0)
+        out_hh = self.weight_hh[0] * out_Vert_h + self.weight_hh[1] * out_Horn_h
+        self.weight_ll.data = F.softmax(self.weight_ll, dim=0)
+        out_ll = self.weight_ll[0] * out_Vert_l + self.weight_ll[1] * out_Horn_l
+        self.weight_hl.data = F.softmax(self.weight_hl, dim=0)
+        out_hl = self.weight_hl[0] * out_Vert_h + self.weight_hl[1] * out_Horn_l
+        self.weight_lh.data = F.softmax(self.weight_lh, dim=0)
+        out_lh = self.weight_lh[0] * out_Vert_l + self.weight_lh[1] * out_Horn_h
+        # out_hh = self.feature_map_add(self.weight[0], self.weight[1], out_Vert_h, out_Horn_h)
+        # out_ll = self.feature_map_add(self.weight[2], self.weight[3], out_Vert_l, out_Horn_l)
+        # out_hl = self.feature_map_add(self.weight[0], self.weight[3], out_Vert_h, out_Horn_l)
+        # out_lh = self.feature_map_add(self.weight[2], self.weight[1], out_Vert_l, out_Horn_h)
         # out_hh = self.alpha * out_Vert_h + self.beta * out_Horn_h
         # out_ll = self.delta * out_Vert_l + self.gamma * out_Horn_l
         # out_hl = self.alpha * out_Vert_h + self.gamma * out_Horn_l
--- ResNet_slim.py
+++ ResNet_slim.py
@@ -1,20 +1,40 @@
+'''
+Properly implemented ResNet-s for CIFAR10 as described in paper [1].
+The implementation and structure of this file is hugely influenced by [2]
+which is implemented for ImageNet and doesn't have option A for identity.
+Moreover, most of the implementations on the web is copy-paste from
+torchvision's resnet and has wrong number of params.
+Proper ResNet-s for CIFAR10 (for fair comparision and etc.) has following
+number of layers and parameters:
+name      | layers | params
+ResNet20  |    20  | 0.27M
+ResNet32  |    32  | 0.46M
+ResNet44  |    44  | 0.66M
+ResNet56  |    56  | 0.85M
+ResNet110 |   110  |  1.7M
+ResNet1202|  1202  | 19.4m
+which this implementation indeed has.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+[2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+If you use this implementation in you work, please don't forget to mention the
+author, Yerlan Idelbayev.
+'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
-from MfA_Conv import MfA_Conv
 
 from torch.autograd import Variable
 
 __all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
 
-
 def _weights_init(m):
     classname = m.__class__.__name__
-    # print(classname)
+    #print(classname)
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
-
 
 class LambdaLayer(nn.Module):
     def __init__(self, lambd):
@@ -30,38 +50,29 @@ class BasicBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1, option='A'):
         super(BasicBlock, self).__init__()
-
-        self.conv1 = nn.Sequential(MfA_Conv(in_planes, planes, high_L=3, high_S=1, low_L=3, low_S=3,
-                                            high_stride=stride, low_stride=stride, basic_stride=stride), )
-        self.bn1 = nn.BatchNorm2d(planes * 4)
-        self.conv2 = nn.Sequential(MfA_Conv(planes * 4, planes, high_L=3, high_S=1, low_L=3, low_S=3), )
-        self.bn2 = nn.BatchNorm2d(planes * 4)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != planes * 4:
+        if stride != 1 or in_planes != planes:
             if option == 'A':
                 """
                 For CIFAR10 ResNet paper uses option A.
                 """
                 self.shortcut = LambdaLayer(lambda x:
-                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes, planes), "constant", 0))
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
             elif option == 'B':
                 self.shortcut = nn.Sequential(
-                    nn.Conv2d(in_planes, self.expansion * planes * 4, kernel_size=1, stride=stride, bias=False),
-                    nn.BatchNorm2d(self.expansion * planes * 4)
-                )
-            elif option == 'C':
-                self.shortcut = nn.Sequential(
-                    MfA_Conv(in_planes, planes, high_stride=stride, low_stride=stride, basic_stride=stride),
-                    nn.BatchNorm2d(planes * 4)
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
                 )
 
     def forward(self, x):
-        # import pdb;pdb.set_trace()
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        short_cut = self.shortcut(x)
-        out += short_cut
+        out += self.shortcut(x)
         out = F.relu(out)
         return out
 
@@ -71,21 +82,21 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 16
 
-        self.conv1 = MfA_Conv(3, 4, high_L=3, high_S=1, low_L=3, low_S=3)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 4, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 8, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 16, num_blocks[2], stride=2)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
         self.linear = nn.Linear(64, num_classes)
 
         self.apply(_weights_init)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
+        strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion * 4
+            self.in_planes = planes * block.expansion
 
         return nn.Sequential(*layers)
 
@@ -131,7 +142,7 @@ def test(net):
     for x in filter(lambda p: p.requires_grad, net.parameters()):
         total_params += np.prod(x.data.numpy().shape)
     print("Total number of params", total_params)
-    print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size()) > 1, net.parameters()))))
+    print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters()))))
 
 
 if __name__ == "__main__":
